9bc5229d6d0efd3281cdf3996265bd2c6c961623
Adrien Perez <adrien.perez789@gmail.com>
2019-12-20 12:09:33 +0100

    boucle ok

--- src/options/Table.jsx
+++ src/options/Table.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Table, Icon } from 'react-materialize';
 import { faCheck, faHandMiddleFinger } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import Axios from "axios";
 
 
 
 function Tableau() {
-  
+    const[users, setUsers] = useState([])
+    const[signs, setSigns] = useState([])
+    
+    useEffect(()=>{
+      Axios.get("http://82.64.46.169:8001/users")
+      .then(res=>{        
+        setUsers(res.data)
+      })
+    }, [])
+
+    useEffect(()=>{
+      Axios.get("http://82.64.46.169:8001/signs")
+      .then(res=>{        
+        setSigns(res.data)
+      })
+    }, [])
+
     return (
-      <Table className="responsive">
+    <>  
+    
+    <Table className="responsive">
         <caption>Feuille d'émargement hebdomadaire</caption>
       <thead>
         <tr>
+        
+          <th colspan="2" className="center-align"></th>
           <th colspan="2" className="center-align">Lundi</th>
           <th colspan="2" className="center-align"> Mardi</th>
           <th colspan="2" className="center-align">Mercredi</th>
@@ -22,6 +43,8 @@ function Tableau() {
       </thead>
       <thead>
         <tr>
+          <th colspan="1" className="center-align">Nom</th>
+          <th colspan="1" className="center-align">Prénom</th>
           <th >AM</th>
           <th >PM</th>
           <th >AM</th>
@@ -35,7 +58,30 @@ function Tableau() {
         </tr>
       </thead>
       <tbody>
+        
+      {users.map(user => {
+          return(       
         <tr>
+        
+          <td className="center-align">
+          {user.lastName}
+          </td>
+          <td className="center-align">
+          {user.firstName}
+          </td>
+
+          <td>
+
+          <Icon >
+              <FontAwesomeIcon icon={faCheck} />
+          </Icon>
+          </td>
+          <td>
+          <Icon >
+              <FontAwesomeIcon icon={faHandMiddleFinger} />
+          </Icon>
+          </td>
+
           <td>
           <Icon >
               <FontAwesomeIcon icon={faCheck} />
@@ -46,19 +92,47 @@ function Tableau() {
               <FontAwesomeIcon icon={faHandMiddleFinger} />
           </Icon>
           </td>
+
           <td>
-            
+          <Icon >
+              <FontAwesomeIcon icon={faCheck} />
+          </Icon>
           </td>
           <td>
-            
+          <Icon >
+              <FontAwesomeIcon icon={faHandMiddleFinger} />
+          </Icon>
+          </td>
+
+          <td>
+          <Icon >
+              <FontAwesomeIcon icon={faCheck} />
+          </Icon>
           </td>
           <td>
-            
+          <Icon >
+              <FontAwesomeIcon icon={faHandMiddleFinger} />
+          </Icon>
           </td>
+
+          <td>
+          <Icon >
+              <FontAwesomeIcon icon={faCheck} />
+          </Icon>
+          </td>
+          <td>
+          <Icon >
+              <FontAwesomeIcon icon={faHandMiddleFinger} />
+          </Icon>
+          </td>
+          
         </tr>
+          )
+        }) }
+
       </tbody>
     </Table>
+    </>
     )
 }
-
 export default Tableau 
