bd58e37eef06149730f6a6e263a957c94c69a333
D <lorrainedams@me.com>
2019-12-20 05:10:47 +0100

    loginadmin

--- src/admin/LoginAdmin.jsx
+++ src/admin/LoginAdmin.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import Admin from "./Admin";
+import { Row, Col, TextInput, Button, Icon } from "react-materialize";
+import Container from "react-materialize/lib/Container";
 
 export default function LoginAdmin() {
-  return <Admin />;
+  return (
+    <>
+      <Container style={{ marginTop: "250px" }}>
+        <TextInput label="First Name" />
+
+        <TextInput label="Password" password />
+
+        <Row style={{ textAlign: "center" }}>
+          <Button node="button" type="submit" waves="light">
+            Submit
+            <Icon right>send</Icon>
+          </Button>
+        </Row>
+      </Container>
+    </>
+  );
 }
